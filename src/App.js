2b5393595c2abe5225713e8e09870e327f9e279f
mathesbh <mathesbh@gmail.com>
2020-11-10 14:56:05 -0300

    Including app component with hooks

--- src/App.js
+++ src/App.js
@@ -1,8 +1,45 @@
+import React, { useState } from 'react'
+import './App.css'
+import { Tab } from './components/Tab';
 
-function App() {
+const tabItems = [
+  {
+    id: 1,
+    title: 'Tab 01',
+    content: 'Tab 01 ativa',
+  },
+  {
+    id: 2,
+    title: 'Tab 02',
+    content: 'Tab 02 ativa',
+  },
+  {
+    id: 3,
+    title: 'Tab 03',
+    content: 'Tab 03 ativa',
+  },
+];
+
+export default function App() {
+  const [active, setActive] = useState(0);
+
   return (
-    <h1>hello world</h1>
-  );
+    <div className="container">
+      <div className="tabs">
+        {tabItems.map(({ id, title }) => (
+        <Tab
+          key={title}
+          title={title}
+          onItemClicked={() => setActive(id)}
+          isActive={active === id}
+        />
+        ))}
+      </div>
+      <div className="content">
+        {tabItems.map(({ id, content }) => {
+          return active === id ? content : ''
+        })}
+      </div>
+    </div>
+  )
 }
-
-export default App;
